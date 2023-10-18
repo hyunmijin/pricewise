3874e353c67bf645421023b15e4373d9237a1169
hyunmi <hyunmi961119@gmail.com>
2023-10-18 15:34:23 +0900

    23.10.18

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,37 +2,37 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 const navIcons = [
-  { src: 'assets/icons/search.svg', alt: 'search' },
-  { src: 'assets/icons/black-heart.svg', alt: 'heart' },
-  { src: 'assets/icons/user.svg', alt: 'user' },
+  { src: '/assets/icons/search.svg', alt: 'search' },
+  { src: '/assets/icons/black-heart.svg', alt: 'heart' },
+  { src: '/assets/icons/user.svg', alt: 'user' },
 ]
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <header className='w-full'>
-      <nav className='nav'>
-        <Link href='/' className='flex items-center gap-1'>
-          <Image
-            src='/assets/icons/logo.svg'
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image 
+            src="/assets/icons/logo.svg"
             width={27}
             height={27}
-            alt='logo'
+            alt="logo"
           />
 
-          <p className='nav-logo'>
-            Price<span className='text-primary'>wise</span>
+          <p className="nav-logo">
+            Price<span className='text-primary'>Wise</span>
           </p>
         </Link>
 
-        <div className='flex items-center gap-5'>
+        <div className="flex items-center gap-5">
           {navIcons.map((icon) => (
-            <Image
+            <Image 
               key={icon.alt}
-              src={icon.alt}
+              src={icon.src}
               alt={icon.alt}
               width={28}
               height={28}
-              className='object-contain'
+              className="object-contain"
             />
           ))}
         </div>
